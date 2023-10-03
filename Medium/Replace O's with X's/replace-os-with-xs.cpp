098e5d56739662c70f7a1bacd6bc7c28b562e3c3
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2023-10-03 19:12:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -7,68 +7,60 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// User function Template for C++
+
 class Solution{
-private:
-    void dfs(int row, int col, vector<vector<int>> &vis, 
-    vector<vector<char>> &mat, int delrow[], int delcol[]) {
-        vis[row][col] = 1; 
-        int n = mat.size();
-        int m = mat[0].size();
-        
-        // check for top, right, bottom, left 
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i]; 
-            // check for valid coordinates and unvisited Os
-            if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m 
-            && !vis[nrow][ncol] && mat[nrow][ncol] == 'O') {
-                dfs(nrow, ncol, vis, mat, delrow, delcol); 
-            }
-        }
-    }
+    private:
+     void dfs(int r,int c,vector<vector<int>> &vis,vector<vector<char>>& mat,int delrow[],int delcol[]){
+         vis[r][c] = 1;
+         int n = mat.size();
+         int m = mat[0].size();
+         for(int i =0;i<4;i++){
+            int nr = r+delrow[i];
+            int nc = c+delcol[i];
+            if(nr>=0 && nr<n && nc>=0&& nc<m  && !vis[nr][nc] && mat[nr][nc] == 'O'){
+                     dfs(nr, nc, vis, mat, delrow, delcol);
+                 }
+             }
+         }
+     
 public:
-    vector<vector<char>> fill(int n, int m, 
-    vector<vector<char>> mat)
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1}; 
-        vector<vector<int>> vis(n, vector<int>(m,0)); 
-        // traverse first row and last row 
-        for(int j = 0 ; j<m;j++) {
-            // check for unvisited Os in the boundary rows
-            // first row 
-            if(!vis[0][j] && mat[0][j] == 'O') {
-                dfs(0, j, vis, mat, delrow, delcol); 
-            }
+        vector<vector<int>> vis(n,vector<int>(m,0));
+         int delrow[] = {-1,0,1,0};
+         int delcol[] = {0,1,0,-1};
+        
+        // boundary traversal check for zero
+        
+        for(int i = 0;i<m;i++){
             
-            // last row 
-            if(!vis[n-1][j] && mat[n-1][j] == 'O') {
-                dfs(n-1,j,vis,mat, delrow, delcol); 
+            if(!vis[0][i] && mat[0][i] == 'O'){
+                dfs(0,i,vis,mat,delrow,delcol);
+            }
+            if(!vis[n-1][i] && mat[n-1][i] == 'O'){
+               dfs(n-1,i,vis,mat,delrow,delcol);
+            }
+           
+        }
+        for(int i = 0;i<n;i++){
+             if(  !vis[i][0]&&mat[i][0] == 'O' ){
+                dfs(i,0,vis,mat,delrow,delcol);
+               }
+            if( !vis[i][m-1]&&mat[i][m-1] == 'O'){
+                dfs(i,m-1,vis,mat,delrow,delcol);
             }
         }
         
-        for(int i = 0;i<n;i++) {
-            // check for unvisited Os in the boundary columns
-            // first column 
-            if(!vis[i][0] && mat[i][0] == 'O') {
-                dfs(i, 0, vis, mat, delrow, delcol); 
-            }
-            
-            // last column
-            if(!vis[i][m-1] && mat[i][m-1] == 'O') {
-                dfs(i, m-1, vis, mat, delrow, delcol); 
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(vis[i][j] == 0 && mat[i][j] == 'O'){
+                    mat[i][j] = 'X';
+                }
             }
         }
+        return mat;
         
-        // if unvisited O then convert to X
-        for(int i = 0;i<n;i++) {
-            for(int j= 0 ;j<m;j++) {
-                if(!vis[i][j] && mat[i][j] == 'O') 
-                    mat[i][j] = 'X'; 
-            }
-        }
-        
-        return mat; 
     }
 };
 
