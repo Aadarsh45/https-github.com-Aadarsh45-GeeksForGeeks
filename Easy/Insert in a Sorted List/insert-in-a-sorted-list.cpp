b79fcbe04b0a4975b6a17249eac89cfa8e218c14
Aadarsh Chaurasia <113307677+Aadarsh45@users.noreply.github.com>
2023-10-08 02:46:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+      
+       Node* cur = head;
+        if(data < cur->data)
+        {
+            Node* temp = new Node(data);
+            temp->next = head;
+            return temp;
+        }
+         Node* temp = NULL;
+        while(cur->next)
+        {
+            if(data < cur->next->data)
+            {
+                Node* next = cur->next;
+                temp = new Node(data);
+                cur->next = temp;
+                temp->next = next;
+                break;
+            }
+            cur = cur->next;
+        }
+        if(!temp)
+            cur->next = new Node(data);
+            
+        return head;
+        
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
